a44e4f5b1e6d682f01d4ef0793ae49c759cd2de1
RobertZK <technoguyrob@gmail.com>
2015-02-21 17:58:37 -0600

    refactor description change test

--- tests/testthat/test-pkgapply.R
+++ tests/testthat/test-pkgapply.R
@@ -1,18 +1,37 @@
 context('pkgapply')
+library(testthatsomemore)
 
 describe("Iterating using a function", {
-  test_that("it can make a trivial change to each package's DESCRIPTION", {
-    with_packages({
-      pkgapply(pkgs, function(pkg) {
-        DESCRIPTION_path <- file.path(pkg$path, 'DESCRIPTION')
-        DESCRIPTION <- readLines(DESCRIPTION_path)
-        DESCRIPTION <- gsub("Depends: R \\([^(]+\\)", "Depends: R (5.0)", DESCRIPTION)
-        writeLines(DESCRIPTION, DESCRIPTION_path)
-      })
+  test_that("it does nothing when passed no packages", {
+    assert(pkgapply(c(), identity))
+  })
 
-      for (pkg in pkgs) {
-        expect_equal(devtools::as.package(pkg)$depends, "R (5.0)")
-      }
-    })
+  test_that("it errors when given a non character package", {
+    expect_error(pkgapply(5, identity))
+  })
+
+  test_that("it errors when the package doesn't exist", {
+    expect_error(pkgapply("boop", identity), "No such package")
+  })
+
+  change_description <- function(...) {
+    eval.parent(substitute({
+      with_packages({
+        pkgapply(..., function(pkg) {
+          DESCRIPTION_path <- file.path(pkg$path, 'DESCRIPTION')
+          DESCRIPTION <- readLines(DESCRIPTION_path)
+          DESCRIPTION <- gsub("Depends: R \\([^(]+\\)", "Depends: R (5.0)", DESCRIPTION)
+          writeLines(DESCRIPTION, DESCRIPTION_path)
+        })
+
+        for (pkg in pkgs) {
+          expect_equal(devtools::as.package(pkg)$depends, "R (5.0)")
+        }
+      })
+    }))
+  }
+
+  test_that("it can make a trivial change to each package's DESCRIPTION", {
+    change_description(pkgs)
   })
 })
