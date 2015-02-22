6e6a61312c99588a3374797f5fbd1fe8f12d219c
RobertZK <technoguyrob@gmail.com>
2015-02-21 18:22:40 -0600

    add describe block

--- tests/testthat/test-pkgapply.R
+++ tests/testthat/test-pkgapply.R
@@ -1,7 +1,7 @@
 context('pkgapply')
 library(testthatsomemore)
 
-describe("Iterating using a function", {
+describe("iterating using a function", {
   test_that("it does nothing when passed no packages", {
     assert(pkgapply(c(), identity))
   })
@@ -14,45 +14,47 @@ describe("Iterating using a function", {
     expect_error(pkgapply("boop", identity), "No such package")
   })
 
-  change_description <- function(..., check = pkgs, prelude = NULL) {
-    eval.parent(substitute({
-      with_packages({
-        prelude
-        pkgapply(..., function(pkg) {
-          DESCRIPTION_path <- file.path(pkg$path, 'DESCRIPTION')
-          DESCRIPTION <- readLines(DESCRIPTION_path)
-          DESCRIPTION <- gsub("Depends: R \\([^(]+\\)", "Depends: R (5.0)", DESCRIPTION)
-          writeLines(DESCRIPTION, DESCRIPTION_path)
+  describe("changing the DESCRIPTION file", {
+    change_description <- function(..., check = pkgs, prelude = NULL) {
+      eval.parent(substitute({
+        with_packages({
+          prelude
+          pkgapply(..., function(pkg) {
+            DESCRIPTION_path <- file.path(pkg$path, 'DESCRIPTION')
+            DESCRIPTION <- readLines(DESCRIPTION_path)
+            DESCRIPTION <- gsub("Depends: R \\([^(]+\\)", "Depends: R (5.0)", DESCRIPTION)
+            writeLines(DESCRIPTION, DESCRIPTION_path)
+          })
+
+          for (pkg in check) {
+            expect_equal(devtools::as.package(pkg)$depends, "R (5.0)")
+          }
         })
+      }))
+    }
 
-        for (pkg in check) {
-          expect_equal(devtools::as.package(pkg)$depends, "R (5.0)")
-        }
-      })
-    }))
-  }
+    test_that("it can make a trivial change to each package's DESCRIPTION", {
+      change_description(pkgs)
+    })
 
-  test_that("it can make a trivial change to each package's DESCRIPTION", {
-    change_description(pkgs)
-  })
+    test_that("it can focus on the packages it is told to focus on", {
+      change_description(p <- file.path(dirname(pkgs[1]), c("package1", "package3")), check = p)
+    })
 
-  test_that("it can focus on the packages it is told to focus on", {
-    change_description(p <- file.path(dirname(pkgs[1]), c("package1", "package3")), check = p)
-  })
+    test_that("it can be given an explicit dir", {
+      change_description(p <- c("package1", "package3"),
+        dir = dir <- dirname(pkgs[1]), check = file.path(dir, p))
+    })
 
-  test_that("it can be given an explicit dir", {
-    change_description(p <- c("package1", "package3"),
-      dir = dir <- dirname(pkgs[1]), check = file.path(dir, p))
-  })
+    test_that("it can be given just a dir", {
+      change_description(dir = dir <- dirname(pkgs[1]),
+        check = file.path(dir, paste0("package", 1:3)))
+    })
 
-  test_that("it can be given just a dir", {
-    change_description(dir = dir <- dirname(pkgs[1]),
-      check = file.path(dir, paste0("package", 1:3)))
-  })
-
-  test_that("it can be given just a function", {
-    change_description(check = file.path(dirname(pkgs[1]), paste0("package", 1:3)),
-                       prelude = setwd(dirname(pkgs[1])))
+    test_that("it can be given just a function", {
+      change_description(check = file.path(dirname(pkgs[1]), paste0("package", 1:3)),
+                         prelude = setwd(dirname(pkgs[1])))
+    })
   })
 })
 
